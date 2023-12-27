d31a58f1c76eec9bb7d5f93fc0b99cd871b01db5
nebler <benklernoah@mgail.com>
2023-12-27 23:10:58 +0100

    modules in rust are hard

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+mod resource_handling;
+use crate::resource_handling::handling::*;
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
-mod resource_handler;
+
+pub struct Resource<'a> {
+    url: &'a str,
+    path: &'a str,
+}
+
 fn main() {
+    set_up_server();
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    resource_handler::setUpServer();
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -39,10 +46,18 @@ fn handle_connection(mut stream: TcpStream) {
     let (http_method, resource) = (&first_line[0], &first_line[1]);
     println!("httpMethod: {}, resources: {}", http_method, resource);
     let status_line = "HTTP/1.1 200 OK";
-    let contents = include_str!("./resources/hello.html");
+    let mut contents = "";
+    if (resource == "/") {
+        contents = include_str!("../resources/hello.html");
+        stream.write_all(contents.as_bytes()).unwrap();
+    } else {
+        let path = "./resources/".to_owned() + resource;
+        stream
+            .write_all(&fs::read_to_string(path).unwrap().as_bytes())
+            .unwrap();
+    }
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     println!("Request: {:#?}", http_request);
     println!("{}", response);
-    stream.write_all(contents.as_bytes()).unwrap();
 }
--- src/resource_handling/handling.rs
+++ src/resource_handling/handling.rs
@@ -1,17 +1,12 @@
-use std::{env, fs};
-pub struct Resource<'a> {
-    url: &'a str,
-    path: &'a str,
-}
-
-pub fn setUpServer<'a>() {
+use std::fs;
+pub fn set_up_server<'a>() {
     let resources = find_resources();
     resources.into_iter().for_each(|r| println!("{}", r));
 }
 
 fn find_resources() -> Vec<String> {
     let mut resources: Vec<String> = vec![];
-    let paths = fs::read_dir("./src/resources").unwrap();
+    let paths = fs::read_dir("./resources").unwrap();
     for path in paths {
         let path_temp = path.unwrap().path();
         let test = path_temp.to_str().unwrap();
